a4f7533511f2e6c21c66a739d11e7889f65a4c9b
Gabriel Vellasco de Mattos <53792961+vellascogabriel@users.noreply.github.com>
2024-06-05 08:25:09 -0300

    authentication components

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <p>
-      This is an autheticated route
-    </p>
+    <UserButton afterSignOutUrl="/" />
   );
 }
